9150cec28f262ab6e42d70fa36ba40cac87b3fa6
Yang <ysu2008@gmail.com>
2014-01-21 11:06:29 -0800

    Initial Commit

---
+++ TempConverterTests/TempConverterTests.m
@@ -0,0 +1,34 @@
+//
+//  TempConverterTests.m
+//  TempConverterTests
+//
+//  Created by Yang Su on 1/21/14.
+//  Copyright (c) 2014 Yang Su. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TempConverterTests : XCTestCase
+
+@end
+
+@implementation TempConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
